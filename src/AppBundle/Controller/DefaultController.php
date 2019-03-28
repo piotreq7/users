2bd2aca2213186b7178a97fa23233ec7e3889ccc
psipinski <psipinski@gmail.com>
2019-03-28 12:31:31 +0100

    second commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,21 +1,126 @@
 <?php
 
+//// Include the main Propel script
+//require_once '/vendor/src/propel/runtime/lib/Propel.php';
+//
+//// Initialize Propel with the runtime configuration
+//Propel::init("/path/to/bookstore/build/conf/bookstore-conf.php");
+//
+//// Add the generated 'classes' directory to the include path
+//set_include_path("/path/to/bookstore/build/classes" . PATH_SEPARATOR . get_include_path());
+
+
+
+
+
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+\Propel::init('/home/psipinski/Pobrane/users/src/propel/build/conf/users-conf.php');
+
 
 class DefaultController extends Controller
 {
+//    /**
+//     * @Route("/", name="homepage")
+//     */
+//    public function indexAction()
+//    {
+//
+//        return $this->render('default/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+//        ]);
+//    }
+
     /**
-     * @Route("/", name="homepage")
+     * @Route("/users", name="show_users")
      */
-    public function indexAction(Request $request)
+    public function showAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+//        $user=new \Users();
+//        $user->setLastName('ted');
+//        $user->setFirsName('ted');
+//        $user->setLogin('tedi');
+//        $user->setPassword('tedi');
+//        $user->save();
+
+//        $findUser = \UsersQuery::create()->findOneByFirsName('ted');
+//        $findUser->setFirsName('Kamien');
+//        $findUser->delete();
+
+//
+//        echo "<pre>";
+//        echo $findUser;
+//        echo "</pre>";
+        $users= \UsersQuery::create()->find();
+
+
+
+
+        $cars=[
+
+            ['make'=>'BMW', 'name'=>'X1'],
+            ['make'=>'wv', 'name'=>'passa']
+
+
+        ];
+
+        $form = $this->createFormBuilder()
+            ->add('search',TextType::class,[
+                'constraints'=>
+                    [
+                new NotBlank(),
+                new Length(['min'=>2])
+                    ]
+
+
+            ])
+            ->getform();
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+//            die("form submited");
+
+        }
+
+        return $this->render('default/users.html.twig',
+            [
+                'cars'=>$cars,
+                'users'=>$users,
+
+                'form'=>$form->createView()
+
+            ]);
+
+    }
+
+    /**
+     * @Route("/add_user", name="add_user")
+     */
+    public function addAction()
+    {
+
+        return $this->render('default/add_user.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/edit_user", name="edit_user")
+     */
+    public function editAction()
+    {
+
+        return $this->render('default/add_user.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
 }
---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: psipinski
+ * Date: 28.03.19
+ * Time: 08:45
+ */
+
+// src/AppBundle/Controller/SecurityController.php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
+class SecurityController extends Controller
+{
+
+    /**
+     * @Route("/", name="login")
+     */
+    public function loginAction(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('default/security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+}
